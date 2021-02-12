09e31aa089ed2bcd4a69dce6b95b778d8e583b8c
Daniel S. Costa <daniel.sousa@msn.com>
2021-02-12 17:18:50 -0300

    adicionado alguns media queries

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,12 @@
+let html = `<section>
+                <div>Sobre</div>
+                <div>Produtos</div>
+                <div>Contatos</div>
+            </section>`;
+html = html.split('section').join('ul');
 
+const menu = document.querySelector(".menu-items");
+
+console.log(menu);
+// nav.appendChild(html);
 
